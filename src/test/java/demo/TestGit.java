7c3b8117b2d36ecaed101f82d3779008546bda58
ShwetaH12 <shweta.zagade1209@gmail.com>
2024-10-14 16:19:51 +0530

    pusttwo

--- src/test/java/demo/TestGit.java
+++ src/test/java/demo/TestGit.java
@@ -3,9 +3,8 @@ package demo;
 public class TestGit {
 //test pull
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Test Git bash");
-		
+		//test push
 	}
 
 }
